28c2ae639368fc48447e7627bbefc6335e5bbf25
tomek5100 <tomek.rozycki@student.uj.edu.pl>
2022-03-08 12:49:01 +0100

    zad6 some fixes, zad7 really close to work proper

---
+++ zestaw1/zad7.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+int main()
+{
+    int filedes1;
+    if ((filedes1 = open("tekst2.txt", O_RDONLY, 0644)) == -1)
+    {
+        perror("Open1 error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int filedes2;
+    if ((filedes2 = open("kopia2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
+    {
+        perror("Open2 error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int size = 64;
+    int nbytes = 32;
+    char bufor[size];
+    char bufor2[size];
+    // char output[2 * size];
+    int readed;
+    while ((readed = read(filedes1, bufor, nbytes)) > 0)
+    {
+        // sprawdzam czy w buforze wystepuje znak nowej linii
+        int even = 0;
+        int iter = 0;
+        for (int i = 0; i < readed; i++)
+        {
+            bufor2[iter] = bufor[i];
+            iter++;
+            if (bufor[i] == '\n')
+            {
+                if ((even % 2) == 0)
+                {
+                    if (write(filedes2, bufor2, iter) == -1)
+                    {
+                        perror("Write error\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                memset(bufor2, '\0', iter); // czyscimy bufor
+                iter = 0;
+                even++;
+            }
+        }
+        // sprintf(output, "Zapisano: %s  ||| o wielkosci %d bajtow\n", bufor2, readed);
+        // if (write(STDOUT_FILENO, output, sizeof(char) * strlen(output)) == -1)
+        // {
+        //     perror("Write on screen error\n");
+        //     exit(EXIT_FAILURE);
+        // }
+        memset(bufor, '\0', size); // czyscimy bufor
+    }
+    if (readed == -1)
+    {
+        perror("Read error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // zamykamy deskryptory
+    if (close(filedes1) == -1)
+    {
+        perror("Close1 error\n");
+        exit(EXIT_FAILURE);
+    }
+    if (close(filedes2) == -1)
+    {
+        perror("Close2 error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
