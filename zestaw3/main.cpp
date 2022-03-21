b1f8d89ef286a0dbf81969256114f0f09c4331e8
tomek5100 <tomek.rozycki@student.uj.edu.pl>
2022-03-21 09:44:22 +0100

    some changes

--- zestaw3/main.cpp
+++ zestaw3/main.cpp
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <limits.h>
+#include <iostream>
+#include <conio.h>
+
+int datagram_size = 65507; //for IPv4
 
 int main(int argc, char const *argv[])
 {
@@ -43,22 +48,33 @@ int main(int argc, char const *argv[])
 
     // tutaj ważne żeby lepiej nie używać char *hello bo to oznacza, że tworzymy wskaźnik na jakąś komórkę w pamięci,
     // jeśli wiemy że jest tam char to możemy jego dlugość sprawdzić strlen(), jeśli nie to mamy problem
-    char hello[13] = "Hello world\n";
+    // char bufor[__INT32_MAX__]; //jest to rozmiar unsigned int
+    // char answear[__INT32_MAX__];
+    char bufor[1000];
+    string bufor;
+    char answear[1000];
 
     while (1)
     {
         struct sockaddr_in klient;
         socklen_t klientRozmiar = sizeof(klient);
 
-        // odbieramy pusty datagram od klienta
-        if (recvfrom(server_fd, NULL, 0, 0, (struct sockaddr *)&klient, &klientRozmiar) == -1)
+        // odbieramy datagram od klienta
+        if (recvfrom(server_fd, bufoor, sizeof(bufor), 0, (struct sockaddr *)&klient, &klientRozmiar) == -1)
         {
             perror("recvfrom() failed");
             exit(EXIT_FAILURE);
         }
 
+        //liczby będę odczytywać wykonując operację modulo 48
+        //przykładowo '1' ma wartość 49 czyli 49 mod 48 => 1 itd
+
+
+
+
+
         // wysyłamy dane do klienta
-        if (sendto(server_fd, hello, sizeof(hello), 0, (struct sockaddr *)&klient, klientRozmiar) == -1)
+        if (sendto(server_fd, answear, sizeof(answear), 0, (struct sockaddr *)&klient, klientRozmiar) == -1)
         {
             perror("sendto() failed");
             exit(EXIT_FAILURE);
